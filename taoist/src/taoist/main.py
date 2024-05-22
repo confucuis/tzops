e1d898b24f25845bbe669a4120d3cf048ed5ec10
confucuis <confucuis@github.com>
2024-05-22 10:24:18 +0800

    ..

--- taoist/src/taoist/main.py
+++ taoist/src/taoist/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import asyncio
+import logging
+import uvicorn
 
+from fastapi import FastAPI
 from taoist.router import user
 
 
@@ -7,8 +10,28 @@ app = FastAPI(debug=True)
 
 app.include_router(user.router)
 
+TAOIST = """
+@@@@@@@@@@@@       @@@@@       @@@@@@@@@@@      @@@     @@@@@@@@@@@ @@@@@@@@@@@@ 
+@@@@@@@@@@@@      @@@ @@@      @@@@@@@@@@@              @@@@@@@@@@@ @@@@@@@@@@@@
+    @@@@         @@@   @@@     @@@     @@@  @@@@@@@@@@@ @@@@            @@@@    
+    @@@@        @@@@@@@@@@@    @@@     @@@    @@@@@@@   @@@@@@@@@@@     @@@@
+    @@@@       @@@@@@@@@@@@@   @@@     @@@     @@@@@           @@@@     @@@@
+    @@@@      @@@         @@@  @@@@@@@@@@@    @@@@@@@   @@@@@@@@@@@     @@@@
+    @@@@     @@@@@       @@@@@ @@@@@@@@@@@  @@@@@@@@@@@ @@@@@@@@@@@     @@@@
+"""
+
 
 def main():
-    import uvicorn
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
-    uvicorn.run("main:app", host="0.0.0.0", port=7000)
+    logger.info(TAOIST)
+
+    try:
+        uvicorn.run("taoist.main:app", host="0.0.0.0", port=7000)
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
