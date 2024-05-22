dda6266742f8a69823d56c44e23b32e5be331a69
confucuis <confucuis@github.com>
2024-05-22 13:35:42 +0800

    ..

--- taoist/src/taoist/log.py
+++ taoist/src/taoist/log.py
@@ -26,3 +26,15 @@ UVICORN_CONFIG["loggers"]["uvicorn.access"]["level"] = "INFO"
 UVICORN_CONFIG["loggers"]["uvicorn.error"]["propagate"] = False
 UVICORN_CONFIG["loggers"]["uvicorn.error"]["handlers"] = ["default"]
 UVICORN_CONFIG["loggers"]["uvicorn.error"]["level"] = "ERROR"
+
+
+class Color:
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    RESET = "\033[0m"  # 重置颜色
--- taoist/src/taoist/main.py
+++ taoist/src/taoist/main.py
@@ -4,28 +4,28 @@ import uvicorn
 from fastapi import FastAPI
 import uvicorn.config
 import uvicorn.server
-from taoist.router import user
-from taoist.log import UVICORN_CONFIG, logger
+from taoist.router import user, role
+from taoist.log import UVICORN_CONFIG, logger, Color
 
 
 app = FastAPI(debug=True)
 
 app.include_router(user.router)
-
+app.include_router(role.router)
 
 TAOIST = """
 @@@@@@@@@@@@       @@@@@       @@@@@@@@@@@      @@@     @@@@@@@@@@@ @@@@@@@@@@@@ 
-@@@@@@@@@@@@      @@@ @@@      @@@@@@@@@@@              @@@@@@@@@@@ @@@@@@@@@@@@
-    @@@@         @@@   @@@     @@@     @@@  @@@@@@@@@@@ @@@@            @@@@    
-    @@@@        @@@@@@@@@@@    @@@     @@@    @@@@@@@   @@@@@@@@@@@     @@@@
-    @@@@       @@@@@@@@@@@@@   @@@     @@@     @@@@@           @@@@     @@@@
+@@@@@@@@@@@@      @@@ @@@      @@@@@@@@@@@  @@@@@@@@@@@ @@@@@@@@@@@ @@@@@@@@@@@@
+    @@@@         @@@   @@@     @@@     @@@    @@@@@@@   @@@@@           @@@@    
+    @@@@        @@@@@@@@@@@    @@@     @@@     @@@@@    @@@@@@@@@@@     @@@@
+    @@@@       @@@@@@@@@@@@@   @@@     @@@     @@@@@          @@@@@     @@@@
     @@@@      @@@         @@@  @@@@@@@@@@@    @@@@@@@   @@@@@@@@@@@     @@@@
     @@@@     @@@@@       @@@@@ @@@@@@@@@@@  @@@@@@@@@@@ @@@@@@@@@@@     @@@@
 """
 
 
 def main():
-    logger.info(TAOIST)
+    logger.info(Color.YELLOW + TAOIST + Color.RESET)
 
     setting = uvicorn.Config(
         "taoist.main:app",
